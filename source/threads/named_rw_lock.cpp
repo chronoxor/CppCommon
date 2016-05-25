d85ed77c7a2ce9e800035f1073706e5e1d2caeea
Ivan Shynkarenka <chronoxor@gmail.com>
2016-05-25 14:31:02 +0300

    Add named rw lock unit test

--- source/threads/named_rw_lock.cpp
+++ source/threads/named_rw_lock.cpp
@@ -23,6 +23,60 @@
 
 namespace CppCommon {
 
+//! Named read/write lock synchronization primitive implementation
+/*!
+    Resource lock value width: 32 bits.
+    Lock owned (either exclusive or shared): L (1 bit).
+    Exclusive waking: W (1 bit).
+    Shared owners count: SC (10 bits).
+    Shared waiters count: SW (10 bits).
+    Exclusive waiters count: EW (10 bits).
+
+    Acquire exclusive:
+    {L=0,W=0,SC=0,SW,EW=0} -> {L=1,W=0,SC=0,SW,EW=0}
+    {L=0,W=1,SC=0,SW,EW} or {L=1,W,SC,SW,EW} ->
+        {L,W,SC,SW,EW+1},
+        wait on event,
+        {L=0,W=1,SC=0,SW,EW} -> {L=1,W=0,SC=0,SW,EW}
+
+    Acquire shared:
+    {L=0,W=0,SC=0,SW,EW=0} -> {L=1,W=0,SC=1,SW,EW=0}
+    {L=1,W=0,SC>0,SW,EW=0} -> {L=1,W=0,SC+1,SW,EW=0}
+    {L=1,W=0,SC=0,SW,EW=0} or {L,W=1,SC,SW,EW} or
+        {L,W,SC,SW,EW>0} -> {L,W,SC,SW+1,EW},
+        wait on event,
+        retry.
+
+    Release exclusive:
+    {L=1,W=0,SC=0,SW,EW>0} ->
+        {L=0,W=1,SC=0,SW,EW-1},
+        release one exclusive waiter.
+    {L=1,W=0,SC=0,SW,EW=0} ->
+        {L=0,W=0,SC=0,SW=0,EW=0},
+        release all shared waiters.
+
+    Note that we never do a direct acquire when W=1
+    (i.e. L=0 if W=1), so here we don't have to check
+    the value of W.
+
+    Release shared:
+    {L=1,W=0,SC>1,SW,EW} -> {L=1,W=0,SC-1,SW,EW}
+    {L=1,W=0,SC=1,SW,EW=0} -> {L=0,W=0,SC=0,SW,EW=0}
+    {L=1,W=0,SC=1,SW,EW>0} ->
+        {L=0,W=1,SC=0,SW,EW-1},
+        release one exclusive waiter.
+
+    Again, we don't need to check the value of W.
+
+    Convert exclusive to shared:
+    {L=1,W=0,SC=0,SW,EW} ->
+        {L=1,W=0,SC=1,SW=0,EW},
+        release all shared waiters.
+
+    Convert shared to exclusive:
+    {L=1,W=0,SC=1,SW,EW} ->
+        {L=1,W=0,SC=0,SW,EW}
+*/
 class NamedRWLock::Impl
 {
     const uint32_t LOCK_OWNED = 0x1;
@@ -43,7 +97,7 @@ class NamedRWLock::Impl
     const uint32_t LOCK_EXCLUSIVE_MASK = (LOCK_EXCLUSIVE_WAKING | (LOCK_EXCLUSIVE_WAITERS_MASK << LOCK_EXCLUSIVE_WAITERS_SHIFT));
 
 public:
-    Impl(const std::string& name) : _value(name)
+    Impl(const std::string& name, uint32_t spin) : _value(name), _spin(spin)
     {
         // Owner of the read/write lock should initialize its value
         if (_value.owner())
@@ -119,7 +173,7 @@ public:
         if (!(value & LOCK_OWNED))
         {
 #if defined(_WIN32) || defined(_WIN64)
-            return InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED + LOCK_SHARED_OWNERS_INC, value) == value;
+            return (InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED + LOCK_SHARED_OWNERS_INC, value) == value);
 #elif defined(unix) || defined(__unix) || defined(__unix__)
             return __sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_OWNED + LOCK_SHARED_OWNERS_INC);
 #endif
@@ -127,7 +181,7 @@ public:
         else if ((value >> LOCK_SHARED_OWNERS_SHIFT) & LOCK_SHARED_OWNERS_MASK)
         {
 #if defined(_WIN32) || defined(_WIN64)
-            return InterlockedCompareExchange(_value.ptr(), value + LOCK_SHARED_OWNERS_INC, value) == value;
+            return (InterlockedCompareExchange(_value.ptr(), value + LOCK_SHARED_OWNERS_INC, value) == value);
 #elif defined(unix) || defined(__unix) || defined(__unix__)
             return __sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_SHARED_OWNERS_INC);
 #endif
@@ -144,30 +198,155 @@ public:
             return false;
 
 #if defined(_WIN32) || defined(_WIN64)
-        return InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED, value) == value;
+        return (InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED, value) == value);
 #elif defined(unix) || defined(__unix) || defined(__unix__)
         return __sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_OWNED);
 #endif
     }
 
+    bool TryConvertWriteToRead()
+    {
+        while (true)
+        {
+            uint32_t value = *_value;
+
+            // Can't convert if there are other shared owners
+            if (((value >> LOCK_SHARED_OWNERS_SHIFT) & LOCK_SHARED_OWNERS_MASK) != 1)
+                return false;
+
+#if defined(_WIN32) || defined(_WIN64)
+            if (InterlockedCompareExchange(_value.ptr(), value - LOCK_SHARED_OWNERS_INC, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+            if (__sync_bool_compare_and_swap(_value.ptr(), value, value - LOCK_SHARED_OWNERS_INC))
+#endif
+                return true;
+        }
+    }
+
     void LockRead()
     {
+        uint32_t spin = 0;
 
+        while (true)
+        {
+            uint32_t value = *_value;
+
+            // Case 1: lock not owned AND no exclusive waiter is waking up AND
+            // there are no shared owners AND there are no exclusive waiters
+            if (!(value & (LOCK_OWNED | (LOCK_SHARED_OWNERS_MASK << LOCK_SHARED_OWNERS_SHIFT) | LOCK_EXCLUSIVE_MASK)))
+            {
+#if defined(_WIN32) || defined(_WIN64)
+                if (InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED + LOCK_SHARED_OWNERS_INC, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                if (__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_OWNED + LOCK_SHARED_OWNERS_INC))
+#endif
+                    break;
+            }
+            // Case 2: lock is owned AND no exclusive waiter is waking up AND
+            // there are shared owners AND there are no exclusive waiters
+            else if ((value & LOCK_OWNED) && ((value >> LOCK_SHARED_OWNERS_SHIFT) & LOCK_SHARED_OWNERS_MASK) > 0 && !(value & LOCK_EXCLUSIVE_MASK))
+            {
+#if defined(_WIN32) || defined(_WIN64)
+                if (InterlockedCompareExchange(_value.ptr(), value + LOCK_SHARED_OWNERS_INC, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                if (__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_SHARED_OWNERS_INC))
+#endif
+                    break;
+            }
+            // Other cases
+            else if (spin++ >= _spin)
+            {
+#if defined(_WIN32) || defined(_WIN64)
+                if (InterlockedCompareExchange(_value.ptr(), value + LOCK_SHARED_WAITERS_INC, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                if (__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_SHARED_WAITERS_INC))
+#endif
+                {
+                    // Go to sleep
+#if defined(_WIN32) || defined(_WIN64)
+                    DWORD result = WaitForSingleObject(_shared_wake, INFINITE);
+                    if (result != WAIT_OBJECT_0)
+                        throwex SystemException("Failed to wait for a shared wake semaphore for the named read/write lock!");
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                    int result = sem_wait(_shared_wake);
+                    if (result != 0)
+                        throwex SystemException("Failed to wait for a shared wake semaphore for the named read/write lock!");
+#endif
+                    // Go back and try again
+                    continue;
+                }
+            }
+
+            Thread::Yield();
+        }
     }
 
     void LockWrite()
     {
+        uint32_t spin = 0;
 
+        while (true)
+        {
+            uint32_t value = *_value;
+
+            // Case 1: lock not owned AND an exclusive waiter is not waking up.
+            // Here we don't have to check if there are exclusive waiters, because
+            // if there are the lock would be owned, and we are checking that anyway.
+            if (!(value & (LOCK_OWNED | LOCK_EXCLUSIVE_WAKING)))
+            {
+#if defined(_WIN32) || defined(_WIN64)
+                if (InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                if (__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_OWNED))
+#endif
+                    break;
+            }
+            // Case 2: lock owned OR lock not owned and an exclusive waiter is waking up.
+            // The second case means an exclusive waiter has just been woken up and is
+            // going to acquire the lock. We have to go to sleep to make sure we don't
+            // steal the lock.
+            else if (spin++ >= _spin)
+            {
+#if defined(_WIN32) || defined(_WIN64)
+                if (InterlockedCompareExchange(_value.ptr(), value + LOCK_EXCLUSIVE_WAITERS_INC, value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                if (__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_EXCLUSIVE_WAITERS_INC))
+#endif
+                {
+#if defined(_WIN32) || defined(_WIN64)
+                    DWORD result = WaitForSingleObject(_exclusive_wake, INFINITE);
+                    if (result != WAIT_OBJECT_0)
+                        throwex SystemException("Failed to wait for an exclusive wake semaphore for the named read/write lock!");
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                    int result = sem_wait(_exclusive_wake);
+                    if (result != 0)
+                        throwex SystemException("Failed to wait for an exclusive wake semaphore for the named read/write lock!");
+#endif
+                    // Acquire the lock.
+                    // At this point *no one* should be able to steal the lock from us.
+                    do
+                    {
+                        value = *_value;
+#if defined(_WIN32) || defined(_WIN64)
+                    } while (InterlockedCompareExchange(_value.ptr(), value + LOCK_OWNED - LOCK_EXCLUSIVE_WAKING, value) != value);
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                    } while (!__sync_bool_compare_and_swap(_value.ptr(), value, value + LOCK_OWNED - LOCK_EXCLUSIVE_WAKING));
+#endif
+                    break;
+                }
+            }
+
+            Thread::Yield();
+        }
     }
 
     void UnlockRead()
     {
-        uint32_t value;
-
         while (true)
         {
-            value = *_value;
+            uint32_t value = *_value;
 
+            // Case 1: there are multiple shared owners
             if (((value >> LOCK_SHARED_OWNERS_SHIFT) & LOCK_SHARED_OWNERS_MASK) > 1)
             {
 #if defined(_WIN32) || defined(_WIN64)
@@ -177,6 +356,7 @@ public:
 #endif
                     break;
             }
+            // Case 2: we are the last shared owner AND there are exclusive waiters
             else if ((value >> LOCK_EXCLUSIVE_WAITERS_SHIFT) & LOCK_EXCLUSIVE_WAITERS_MASK)
             {
 #if defined(_WIN32) || defined(_WIN64)
@@ -196,6 +376,7 @@ public:
                     break;
                 }
             }
+            // Case 3: we are the last shared owner AND there are no exclusive waiters
             else
             {
 #if defined(_WIN32) || defined(_WIN64)
@@ -212,12 +393,11 @@ public:
 
     void UnlockWrite()
     {
-        uint32_t value;
-
         while (true)
         {
-            value = *_value;
+            uint32_t value = *_value;
 
+            // Case 1: if we have exclusive waiters, release one
             if ((value >> LOCK_EXCLUSIVE_WAITERS_SHIFT) & LOCK_EXCLUSIVE_WAITERS_MASK)
             {
 #if defined(_WIN32) || defined(_WIN64)
@@ -237,6 +417,7 @@ public:
                     break;
                 }
             }
+            // Case 2: if we have shared waiters, release all of them
             else
             {
                 uint32_t shared_waiters = (value >> LOCK_SHARED_WAITERS_SHIFT) & LOCK_SHARED_WAITERS_MASK;
@@ -269,6 +450,42 @@ public:
         }
     }
 
+    void ConvertWriteToRead()
+    {
+        /// Exclusive acquires are not given a chance to acquire
+        /// the lock before this function does - as a result,
+        /// this function will never block.
+        while (true)
+        {
+            uint32_t value = *_value;
+
+            uint32_t shared_waiters = (value >> LOCK_SHARED_WAITERS_SHIFT) & LOCK_SHARED_WAITERS_MASK;
+
+#if defined(_WIN32) || defined(_WIN64)
+            if (InterlockedCompareExchange(_value.ptr(), (value + LOCK_SHARED_OWNERS_INC) & ~(LOCK_SHARED_WAITERS_MASK << LOCK_SHARED_WAITERS_SHIFT), value) == value)
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+            if (__sync_bool_compare_and_swap(_value.ptr(), value, (value + LOCK_SHARED_OWNERS_INC) & ~(LOCK_SHARED_WAITERS_MASK << LOCK_SHARED_WAITERS_SHIFT)))
+#endif
+            {
+                if (shared_waiters > 0)
+                {
+#if defined(_WIN32) || defined(_WIN64)
+                    if (!ReleaseSemaphore(_shared_wake, shared_waiters, nullptr))
+                        throwex SystemException("Failed to release a shared wake semaphore for the named read/write lock!");
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+                    while (shared_waiters-- > 0)
+                    {
+                        int result = sem_post(_shared_wake);
+                        if (result != 0)
+                            throwex SystemException("Failed to release a shared wake semaphore for the named read/write lock!");
+                    }
+#endif
+                }
+                break;
+            }
+        }
+    }
+
 private:
     SharedType<volatile uint32_t> _value;
 #if defined(_WIN32) || defined(_WIN64)
@@ -278,9 +495,10 @@ private:
     sem_t* _exclusive_wake;
     sem_t* _shared_wake;
 #endif
+    uint32_t _spin;
 };
 
-NamedRWLock::NamedRWLock(const std::string& name) : _pimpl(new Impl(name))
+NamedRWLock::NamedRWLock(const std::string& name) : _pimpl(new Impl(name, 4000))
 {
 }
 
---
+++ tests/test_threads_named_rw_lock.cpp
@@ -0,0 +1,138 @@
+//
+// Created by Ivan Shynkarenka on 25.05.2016.
+//
+
+#include "catch.hpp"
+
+#include "threads/named_rw_lock.h"
+#include "threads/thread.h"
+
+#include <atomic>
+#include <thread>
+#include <vector>
+
+using namespace CppCommon;
+
+TEST_CASE("Named read/write lock", "[CppCommon][Threads]")
+{
+    // Named read/write lock master
+    NamedRWLock master("named_rw_lock_test");
+
+    // Test TryLockRead() method
+    {
+        NamedRWLock slave("named_rw_lock_test");
+        REQUIRE(slave.TryLockRead());
+        REQUIRE(!slave.TryLockWrite());
+        slave.UnlockRead();
+    }
+
+    // Test TryLockWrite() method
+    {
+        NamedRWLock slave("named_rw_lock_test");
+        REQUIRE(slave.TryLockWrite());
+        REQUIRE(!slave.TryLockRead());
+        slave.UnlockWrite();
+    }
+
+    // Test LockRead()/UnlockRead() methods
+    {
+        NamedRWLock slave("named_rw_lock_test");
+        slave.LockRead();
+        REQUIRE(!slave.TryLockWrite());
+        slave.UnlockRead();
+    }
+
+    // Test LockWrite()/UnlockWrite() methods
+    {
+        NamedRWLock slave("named_rw_lock_test");
+        slave.LockWrite();
+        REQUIRE(!slave.TryLockRead());
+        slave.UnlockWrite();
+    }
+}
+
+TEST_CASE("Named read/write locker", "[CppCommon][Threads]")
+{
+    int items_to_produce = 10;
+    int consumers_count = 4;
+    int crc = 0;
+    std::vector<int> crcs;
+    int current = 0;
+
+    // Named read/write lock master
+    NamedRWLock lock("named_rw_lock_test");
+
+    // Reset consumers' results
+    for (int i = 0; i < consumers_count; ++i)
+        crcs.push_back(0);
+
+    // Caclulate result value
+    int result = 0;
+    for (int i = 0; i < items_to_produce; ++i)
+        result += i;
+
+    // Start producer thread
+    std::thread producer = std::thread([&crc, &current, items_to_produce]()
+    {
+        // Named read/write lock slave
+        NamedRWLock lock("named_rw_lock_test");
+
+        for (int i = 0; i < items_to_produce; ++i)
+        {
+            // Use a write locker to produce the item
+            {
+                WriteLocker<NamedRWLock> locker(lock);
+
+                // Update the current produced item and produced crc
+                current = i;
+                crc += current;
+            }
+
+            // Sleep for a while...
+            Thread::SleepFor(std::chrono::milliseconds(10));
+        }
+    });
+
+    // Start consumers threads
+    std::vector<std::thread> consumers;
+    for (int consumer = 0; consumer < consumers_count; ++consumer)
+    {
+        consumers.push_back(std::thread([&crcs, &current, consumer, items_to_produce]()
+        {
+            // Named read/write lock slave
+            NamedRWLock lock("named_rw_lock_test");
+
+            int item = 0;
+            while (item < (items_to_produce - 1))
+            {
+                // Use a read locker to consume the item
+                {
+                    ReadLocker<NamedRWLock> locker(lock);
+
+                    // Check for the current item changed
+                    if (item != current)
+                    {
+                        // Update consumed crc
+                        item = current;
+                        crcs[consumer] += item;
+                    }
+                }
+
+                // Yield to another thread...
+                Thread::Yield();
+            }
+        }));
+    }
+
+    // Wait for producer thread
+    producer.join();
+
+    // Wait for all consumers threads
+    for (auto& consumer : consumers)
+        consumer.join();
+
+    // Check result
+    REQUIRE(crc == result);
+    for (int i = 0; i < consumers_count; ++i)
+        REQUIRE(crcs[i] > 0);
+}
