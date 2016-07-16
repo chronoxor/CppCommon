5acb5c743078176eb05d033e1f6584019300935e
Ivan Shynkarenka <chronoxor@gmail.com>
2016-07-16 04:52:40 +0300

    Linux optimizations in Timestamp::local()

--- source/time/timestamp.cpp
+++ source/time/timestamp.cpp
@@ -56,16 +56,14 @@ uint64_t Timestamp::local()
     if (clock_gettime(CLOCK_REALTIME, &timestamp) != 0)
         throwex SystemException("Cannot get value of CLOCK_REALTIME timer!");
 
-    struct tm result;
+    // Adjust UTC time with local timezone offset
+    struct tm local;
     time_t time = timestamp.tv_sec;
-    if (localtime_r(&time, &result) != &result)
+    if (localtime_r(&time, &local) != &local)
         throwex SystemException("Cannot convert CLOCK_REALTIME time to local date & time structure!");
+    timestamp.tv_sec += local.tm_gmtoff;
 
-    time = timegm(&result);
-    if (time == -1)
-        throwex SystemException("Cannot convert date & time to local timestamp!");
-
-    return (time * 1000 * 1000 * 1000) + timestamp.tv_nsec;
+    return (timestamp.tv_sec * 1000 * 1000 * 1000) + timestamp.tv_nsec;
 #endif
 }
 
