abe7b6fb9d243e79b6b7f7e0b0b07f4eb4ef5245
Ivan Shynkarenka <chronoxor@gmail.com>
2016-08-13 18:20:32 +0300

    Code clean

--- examples/string_encoding.cpp
+++ examples/string_encoding.cpp
@@ -10,18 +10,22 @@
 
 #include <iostream>
 
-// Allow Windows console to output Unicode strings using std::wcout
 #if defined(_WIN32) || defined(_WIN64)
-#include <io.h>
 #include <fcntl.h>
-int mode = _setmode(_fileno(stdout), _O_U16TEXT);
+#include <io.h>
 #elif defined(unix) || defined(__unix) || defined(__unix__)
 #include <locale.h>
-char* locale = setlocale (LC_ALL, "");
 #endif
 
 int main(int argc, char** argv)
 {
+    // Allow the console to output Unicode strings using std::wcout
+#if defined(_WIN32) || defined(_WIN64)
+    _setmode(_fileno(stdout), _O_U16TEXT);
+#elif defined(unix) || defined(__unix) || defined(__unix__)
+    setlocale(LC_ALL, "");
+#endif
+
     std::string utf8 = "\xD0\x9F\xD1\x80\xD0\xB8\xD0\xB2\xD0\xB5\xD1\x82\x20\xD0\xBC\xD0\xB8\xD1\x80\x21";
     std::wstring sys = CppCommon::Encoding::FromUTF8(utf8);
     std::wcout << sys << std::endl;
