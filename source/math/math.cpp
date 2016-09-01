e2a5fc84f7569a48cf86b4d30012eea022c729d8
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-01 03:09:30 +0300

    Bugfix

--- source/math/math.cpp
+++ source/math/math.cpp
@@ -16,7 +16,220 @@ namespace CppCommon {
 
 uint64_t Math::MulDiv64(uint64_t operant, uint64_t multiplier, uint64_t divider)
 {
-#if defined(_MSC_VER) && (_MSC_VER == 1900)
+#if defined(_MSC_VER)
+#if defined(_M_IX86)
+    // Declare 128bit storage
+    struct
+    {
+        unsigned long DW[4];
+    } var128, quotient;
+
+    // Change semantics for intermediate results for Full Div
+    // by renaming the vars
+    #define REMAINDER quotient
+    #define QUOTIENT edi
+
+    // Save combined sign on stack
+    _asm
+    {
+        mov     eax, dword ptr[operant+4]
+        xor     eax, dword ptr[multiplier+4]
+        xor     eax, dword ptr[divider+4]
+        pushfd
+    }
+
+    _asm
+    {
+        // First check divider for 0
+        mov     eax, dword ptr[divider+4]
+        or      eax, dword ptr[divider]
+        jnz     dividerOK
+        div     eax
+dividerOK:
+        lea     edi,[var128]                    // edi = &var128
+        // Check multiplier for 1 or 0
+        xor     eax, eax
+        cmp     eax, dword ptr[multiplier+4]
+        jnz     startMUL
+        cmp     eax, dword ptr[multiplier]
+        jnz     multiNotNUL
+        xor     edx, edx
+        popfd                                   // cleanup stack
+        jmp     done
+multiNotNUL:
+        // Set result HI part to 0
+        xor     eax,eax
+        mov     dword ptr[edi+12], eax
+        mov     dword ptr[edi+8], eax
+        mov     eax, 1
+        cmp     eax, dword ptr[multiplier]
+        jnz     smallMUL
+        // Multiplier is 1 so just copy operant to result
+        mov     eax, dword ptr[operant+4]
+        mov     dword ptr[edi+4], eax
+        mov     eax, dword ptr[operant]
+        mov     dword ptr[edi], eax
+        jmp     startDIV
+smallMUL:
+        // Test for 32/32 bit multiplication
+        xor     eax, eax
+        mov     ecx, dword ptr[operant+4]
+        or      ecx, eax         ;test for both hiwords zero.
+        jnz     startMUL
+        // Do 32/32 bit multiplication
+        mov     ecx, dword ptr[multiplier]
+        mov     eax, dword ptr[operant]
+        mul     ecx
+        mov     dword ptr[edi+4], edx
+        mov     dword ptr[edi], eax
+        jmp     startDIV
+startMUL:
+        // Check signs
+        // Multiply: var128 = operant * multiplier
+        mov     eax, dword ptr[multiplier]      // eax = LO(multiplier)
+        mul     dword ptr[operant]              // edx:eax = eax * LO(operant)
+        mov     dword ptr[edi], eax             // var128.DW0 = eax
+        mov     ecx, edx                        // ecx = edx
+
+        mov     eax, dword ptr[multiplier]      // eax = LO(multiplier)
+        mul     dword ptr[operant+4]            // edx:eax = eax * HI(operant)
+        add     eax, ecx                        // eax = eax + ecx
+        adc     edx, 0                          // edx = edx + 0 + carry
+        mov     ebx, eax
+        mov     ecx, edx
+
+        mov     eax, dword ptr[multiplier+4]
+        mul     dword ptr[operant]
+        add     eax, ebx
+        mov     dword ptr[edi+4], eax
+        adc     ecx, edx
+        pushfd
+
+        mov     eax, dword ptr[multiplier+4]
+        mul     dword ptr[operant+4]
+        popfd
+        adc     eax, ecx
+        adc     edx, 0
+        mov     dword ptr[edi+8], eax
+        mov     dword ptr[edi+12], edx
+startDIV:
+        // Divide: var128 = var128 / divider
+        //
+        // Test divider = 32bit value
+        mov     eax, dword ptr[divider+4]
+        cmp     eax, 0
+        jnz     fullDIV
+        mov     ecx, dword ptr[divider]
+        cmp     ecx, 1
+        jz      applySign
+
+        // Start 128/32 bit division
+        mov     eax, dword ptr[edi+12]
+        xor     edx, edx
+        div     ecx
+        mov     dword ptr[quotient+12], eax
+
+        mov     eax, dword ptr[edi+8]
+        div     ecx
+        mov     dword ptr[quotient+8], eax
+
+        mov     eax, dword ptr[edi+4]
+        div     ecx
+        mov     dword ptr[quotient+4], eax
+
+        mov     eax, dword ptr[edi]
+        div     ecx
+        mov     dword ptr[quotient], eax
+
+        // Copy the quotient to the result storage (var128)
+        mov     eax, dword ptr[quotient+12]
+        mov     dword ptr[edi+12], eax
+        mov     eax, dword ptr[quotient+8]
+        mov     dword ptr[edi+8], eax
+        mov     eax, dword ptr[quotient+4]
+        mov     dword ptr[edi+4], eax
+        mov     eax, dword ptr[quotient]
+        mov     dword ptr[edi], eax
+        // To sign correction and return
+        jmp     applySign
+
+fullDIV:
+        // Full 128/64 bit division
+        xor     eax, eax
+        mov     dword ptr[REMAINDER+12], eax
+        mov     dword ptr[REMAINDER+8], eax
+        mov     dword ptr[REMAINDER+4], eax
+        mov     dword ptr[REMAINDER], eax
+
+        mov     ecx, 128
+loop1:
+        // Compute REMAINDER:QUOTIENT = REMAINDER:QUOTIENT shl 1
+        shl     dword ptr[QUOTIENT], 1
+        rcl     dword ptr[QUOTIENT+4], 1
+        rcl     dword ptr[QUOTIENT+8], 1
+        rcl     dword ptr[QUOTIENT+12], 1
+        rcl     dword ptr[REMAINDER], 1
+        rcl     dword ptr[REMAINDER+4], 1
+        rcl     dword ptr[REMAINDER+8], 1
+        rcl     dword ptr[REMAINDER+12], 1
+
+        // Test (REMAINDER >= Divider)
+        xor     eax, eax
+        cmp     dword ptr[REMAINDER+12], eax
+        ja      iftrue
+        jb      iffalse
+
+        cmp     dword ptr[REMAINDER+8], eax
+        ja      iftrue
+        jb      iffalse
+
+        mov     eax, dword ptr[REMAINDER+4]
+        cmp     eax, dword ptr[divider+4]
+        ja      iftrue
+        jb      iffalse
+
+        mov     eax, dword ptr[REMAINDER]
+        cmp     eax, dword ptr[divider]
+        jb      iffalse
+iftrue:
+        // Remainder = remainder - divider
+        mov     eax, dword ptr[divider]
+        sub     dword ptr[REMAINDER], eax
+        mov     eax, dword ptr[divider+4]
+        sbb     dword ptr[REMAINDER+4], eax
+        xor     eax, eax
+        sbb     dword ptr[REMAINDER+8], eax
+        sbb     dword ptr[REMAINDER+12], eax
+        // Quotient = quotient +1
+        add     dword ptr[QUOTIENT], 1
+        adc     dword ptr[QUOTIENT+4], 0
+        adc     dword ptr[QUOTIENT+8], 0
+        adc     dword ptr[QUOTIENT+12], 0
+iffalse:
+        // Loop size = 101 bytes, is less than 127 so loop is possible
+        loop    loop1
+
+applySign:
+        // Correct the sign of the result based on the stored combined sign
+        popfd
+        jns     storeRes
+        not     dword ptr[edi+12]
+        not     dword ptr[edi+ 8]
+        not     dword ptr[edi+ 4]
+        not     dword ptr[edi]
+        add     dword ptr[edi], 1
+        adc     dword ptr[edi+ 4], 0
+        adc     dword ptr[edi+ 8], 0
+        adc     dword ptr[edi+12], 0
+
+storeRES:
+        // Get low order qword from var128
+        mov     edx, dword ptr[edi+4]
+        mov     eax, dword ptr[edi]
+done:
+    }
+    // result is returned in edx:eax
+#elif defined (_M_X64 )
 #pragma warning(push)
 #pragma warning(disable: 4018)
 #pragma warning(disable: 4244)
@@ -86,6 +299,7 @@ uint64_t Math::MulDiv64(uint64_t operant, uint64_t multiplier, uint64_t divider)
 
     return ((uint64_t)q1 << 32) | q0;
 #pragma warning(pop)
+#endif
 #elif defined(__GNUC__)
     __uint128_t a = operant;
     __uint128_t b = multiplier;
