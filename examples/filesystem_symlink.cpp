c619e5485ad9c8f87becac2cab6e7e3e0aa2d296
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-04 03:50:11 +0300

    Add some examples

---
+++ examples/filesystem_symlink.cpp
@@ -0,0 +1,27 @@
+/*!
+    \file filesystem_symlink.cpp
+    \brief Filesystem symlink wrapper example
+    \author Ivan Shynkarenka
+    \date 04.09.2016
+    \copyright MIT License
+*/
+
+#include "filesystem/symlink.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    CppCommon::Symlink symlink("example.lnk");
+
+    // Create a new symbolic link from the executable path
+    CppCommon::Symlink::CreateSymlink(CppCommon::Path::executable(), symlink);
+
+    // Read symbolic link target
+    std::cout << "Symbolic link target: " << symlink.target() << std::endl;
+
+    // Remove symbolic link
+    CppCommon::Path::Remove(symlink);
+
+    return 0;
+}
