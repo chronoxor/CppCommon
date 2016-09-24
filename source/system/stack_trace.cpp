35a0843d1000b70db7e0d525793550f2b5835354
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-24 04:57:33 +0300

    fix

--- source/system/stack_trace.cpp
+++ source/system/stack_trace.cpp
@@ -15,7 +15,7 @@
 #include <iomanip>
 #include <sstream>
 
-#if defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__) || !defined(__CYGWIN__)
+#if !defined(__CYGWIN__) && defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
 #include <execinfo.h>
 #if defined(LIBBFD_SUPPORT)
 #include <bfd.h>
