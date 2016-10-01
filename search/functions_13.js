9497d534a466c08fea15afae26d708f8b77b13a4
Travis CI <travis@travis-ci.org>
2016-10-01 19:03:24 +0000

    Update generated documentation

--- search/functions_13.js
+++ search/functions_13.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['wait',['Wait',['../class_cpp_common_1_1_barrier.html#ae5d6c33df84832a3b7b3a2aae08ffae7',1,'CppCommon::Barrier::Wait()'],['../class_cpp_common_1_1_event_auto_reset.html#a7284f18bcb4b07f210500911ea5db61b',1,'CppCommon::EventAutoReset::Wait()'],['../class_cpp_common_1_1_event_manual_reset.html#ae4e4c2346a4728ee1337096fe66e369e',1,'CppCommon::EventManualReset::Wait()'],['../class_cpp_common_1_1_latch.html#aed8ac82e2202f1f6ff9a3f903b9e57bb',1,'CppCommon::Latch::Wait()'],['../class_cpp_common_1_1_named_event_auto_reset.html#a250c6fd4dd41b99e135a4d221fbaa0a8',1,'CppCommon::NamedEventAutoReset::Wait()'],['../class_cpp_common_1_1_named_event_manual_reset.html#a9182c98aa3067b63c4e247c49c999fa4',1,'CppCommon::NamedEventManualReset::Wait()'],['../class_cpp_common_1_1_spin_barrier.html#a75fdf0da29eca1cc8921715e6b53c8ea',1,'CppCommon::SpinBarrier::Wait()']]],
-  ['what',['what',['../class_cpp_common_1_1_exception.html#aebb89cc7bd3da6a049b5536d40d84e94',1,'CppCommon::Exception']]],
+  ['what',['what',['../class_cpp_common_1_1_exception.html#aa050971385438783268d9c984e3782c3',1,'CppCommon::Exception']]],
   ['write',['Write',['../class_cpp_common_1_1_file.html#a889d0ff2feb7b614fbf8bdda9eec82bc',1,'CppCommon::File']]],
   ['writeallbytes',['WriteAllBytes',['../class_cpp_common_1_1_file.html#abf4db6626545a31e1e97e2a1f790b560',1,'CppCommon::File']]],
   ['writealllines',['WriteAllLines',['../class_cpp_common_1_1_file.html#a2a9e38a8125f91110ebbf72d0cac60bd',1,'CppCommon::File']]],
