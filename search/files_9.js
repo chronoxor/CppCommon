5a9e8895914d6a3272f9229028296a5d84e1b3e3
Travis CI <travis@travis-ci.org>
2016-10-01 18:41:26 +0000

    Update generated documentation

--- search/files_9.js
+++ search/files_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['resource_2eh',['resource.h',['../resource_8h.html',1,'']]],
   ['rw_5flock_2ecpp',['rw_lock.cpp',['../rw__lock_8cpp.html',1,'']]],
   ['rw_5flock_2eh',['rw_lock.h',['../rw__lock_8h.html',1,'']]]
 ];
