03ecc98096dc66b6876ed9b3be47222475161e56
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-04 01:01:50 +0300

    Bugfixing

--- tests/test_threads_condition_variable.cpp
+++ tests/test_threads_condition_variable.cpp
@@ -52,38 +52,46 @@ TEST_CASE("Condition variable notify one", "[CppCommon][Threads]")
     REQUIRE(result1 == 0);
     REQUIRE(result2 == 0);
 }
-/*
+
 TEST_CASE("Condition variable notify all", "[CppCommon][Threads]")
 {
     int concurrency = 8;
-    int result = 0;
+    int result1 = concurrency;
+    int result2 = concurrency;
 
     CriticalSection cs;
-    ConditionVariable cv;
+    ConditionVariable cv1;
+    ConditionVariable cv2;
 
     // Start threads
     std::vector<std::thread> threads;
     for (int thread = 0; thread < concurrency; ++thread)
     {
-        threads.push_back(std::thread([&cs, &cv, &result]()
+        threads.push_back(std::thread([&cs, &cv1, &cv2, &result1, &result2]()
         {
             cs.Lock();
-            cv.Wait(cs);
-            ++result;
+            --result1;
+            cv1.NotifyOne();
+            cv2.Wait(cs, [&result2]() { return (result2 == 0); });
             cs.Unlock();
         }));
     }
 
-    // Send all-thread notification
-    cs.Lock();
-    cv.NotifyAll();
-    cs.Unlock();
+    // Send all-threads notifications
+    for (int i = 0; i < concurrency; ++i)
+    {
+        cs.Lock();
+        cv1.Wait(cs, [&result1]() { return (result1 == 0); });
+        result2 = 0;
+        cv2.NotifyAll();
+        cs.Unlock();
+    }
 
     // Wait for all threads
     for (auto& thread : threads)
         thread.join();
 
-    // Check result
-    REQUIRE(result == concurrency);
+    // Check results
+    REQUIRE(result1 == 0);
+    REQUIRE(result2 == 0);
 }
-*/
