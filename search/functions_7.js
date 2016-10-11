4556ade00c375802936c6b6dca54e439f5283878
AppVeyor <team@appveyor.com>
2016-10-11 15:14:26 +0000

    Update generated documentation

--- search/functions_7.js
+++ search/functions_7.js
@@ -8,7 +8,7 @@ var searchData=
   ['hasroot',['HasRoot',['../class_cpp_common_1_1_path.html#a2ed7f1b4d423d97bf78465b4eafdb30f',1,'CppCommon::Path']]],
   ['hasstem',['HasStem',['../class_cpp_common_1_1_path.html#a2be62830fe45e1e7aaa76abdd62f5a2e',1,'CppCommon::Path']]],
   ['home',['home',['../class_cpp_common_1_1_path.html#a7f97071e1376a33026fe05599cf0f8d5',1,'CppCommon::Path']]],
-  ['hour',['hour',['../class_cpp_common_1_1_time.html#aa6bf37549c29fa8edcf1ddb03dab973e',1,'CppCommon::Time']]],
-  ['hours',['hours',['../class_cpp_common_1_1_timespan.html#af5aa737510d11db7c38e352cf231b384',1,'CppCommon::Timespan::hours() const noexcept'],['../class_cpp_common_1_1_timespan.html#a81b2ed7a759fb98c8488a0d8c50fa523',1,'CppCommon::Timespan::hours(int64_t hours) noexcept'],['../class_cpp_common_1_1_timestamp.html#ab86196d874346bbae5f31d586448e9c8',1,'CppCommon::Timestamp::hours() const noexcept'],['../class_cpp_common_1_1_timestamp.html#a930864f50feb3c960143490f1542f4dc',1,'CppCommon::Timestamp::hours(int64_t hours) noexcept']]],
+  ['hour',['hour',['../class_cpp_common_1_1_time.html#aed91af4e844238159c5b56ccd91efd27',1,'CppCommon::Time']]],
+  ['hours',['hours',['../class_cpp_common_1_1_timespan.html#acff8cb641601a714872704a72dd956c8',1,'CppCommon::Timespan::hours() const  noexcept'],['../class_cpp_common_1_1_timespan.html#a81b2ed7a759fb98c8488a0d8c50fa523',1,'CppCommon::Timespan::hours(int64_t hours) noexcept'],['../class_cpp_common_1_1_timestamp.html#a4ee4268696f11fef55d0facd2c696eac',1,'CppCommon::Timestamp::hours() const  noexcept'],['../class_cpp_common_1_1_timestamp.html#a930864f50feb3c960143490f1542f4dc',1,'CppCommon::Timestamp::hours(int64_t hours) noexcept']]],
   ['hyperthreading',['HyperThreading',['../class_cpp_common_1_1_c_p_u.html#a27cacc71999419f0bc0c720d6ec60520',1,'CppCommon::CPU']]]
 ];
