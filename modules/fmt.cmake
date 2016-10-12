550ddb922d962f6cce21d27de8ec31da1128fec6
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-13 02:26:33 +0300

    update

--- modules/fmt.cmake
+++ modules/fmt.cmake
@@ -2,12 +2,12 @@ if(NOT TARGET fmt)
 
   # Module library
   file(GLOB SOURCE_FILES "fmt/fmt/*.cc")
-  if(CMAKE_MAKE_PROGRAM MATCHES "(MSBuild|devenv|msdev|nmake)")
+  if(NOT MSVC)
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  else()
     # C4127: conditional expression is constant
     # C4702: unreachable code
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${COMMON_COMPILE_FLAGS} /wd4127 /wd4702")
-  else()
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${COMMON_COMPILE_FLAGS}")
+    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4127 /wd4702")
   endif()
   add_library(fmt ${SOURCE_FILES} ${ASSEMBLER_FILES})
   target_include_directories(fmt PRIVATE "fmt")
