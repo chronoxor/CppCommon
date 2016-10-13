d6755a7f2c85b47b613a96163669dc06e259e4e4
Ivan Shynkarenka <chronoxor@gmail.com>
2016-10-13 03:36:05 +0300

    Clang

--- include/threads/mpsc_ring_buffer.h
+++ include/threads/mpsc_ring_buffer.h
@@ -40,11 +40,11 @@ public:
     */
     explicit MPSCRingBuffer(size_t capacity, size_t concurrency = std::thread::hardware_concurrency());
     MPSCRingBuffer(const MPSCRingBuffer&) = delete;
-    MPSCRingBuffer(MPSCRingBuffer&&) noexcept = default;
+    MPSCRingBuffer(MPSCRingBuffer&&) = default;
     ~MPSCRingBuffer() = default;
 
     MPSCRingBuffer& operator=(const MPSCRingBuffer&) = delete;
-    MPSCRingBuffer& operator=(MPSCRingBuffer&&) noexcept = default;
+    MPSCRingBuffer& operator=(MPSCRingBuffer&&) = default;
 
     //! Is ring buffer empty?
     bool empty() const noexcept { return (size() == 0); }
