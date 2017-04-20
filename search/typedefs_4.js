0fdbc794ff043bb13c2c2e7932284c1996d02bc6
Travis CI <travis@travis-ci.org>
2017-04-20 08:20:20 +0000

    Update generated documentation

--- search/typedefs_4.js
+++ search/typedefs_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['nullallocator',['NullAllocator',['../namespace_cpp_common.html#a1c2d2061065375ccae4411e51a3a695a',1,'CppCommon']]]
+  ['nullallocator',['NullAllocator',['../namespace_cpp_common.html#a42c6953f48a287969adc270f81d01305',1,'CppCommon']]]
 ];
