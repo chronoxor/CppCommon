98fffb110c82efb6e432f59ce5a68c95485a44b4
Travis CI <travis@travis-ci.org>
2017-04-20 08:26:25 +0000

    Update generated documentation

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['barrier',['Barrier',['../class_cpp_common_1_1_barrier.html#a87e728c403f9797c08d16f2d38f0b78f',1,'CppCommon::Barrier::Barrier(int threads)'],['../class_cpp_common_1_1_barrier.html#afd507741119c12feeceaf658a2257118',1,'CppCommon::Barrier::Barrier(const Barrier &amp;)=delete'],['../class_cpp_common_1_1_barrier.html#a5eda0ae3e89070aaface5b6cdc499280',1,'CppCommon::Barrier::Barrier(Barrier &amp;&amp;barrier) noexcept']]],
-  ['begin',['begin',['../class_cpp_common_1_1_directory.html#a10ba7f4da1370eb37141b47388a690c9',1,'CppCommon::Directory']]],
+  ['begin',['begin',['../class_cpp_common_1_1_directory.html#a37594d9e54e928a3eff52c77b6dc4069',1,'CppCommon::Directory']]],
   ['bitset',['bitset',['../class_cpp_common_1_1_flags.html#a0500b70ea7cf23828e6426d212176a72',1,'CppCommon::Flags']]],
   ['buffer',['buffer',['../class_cpp_common_1_1_arena_memory_manager.html#a7a1dc886256387eb2f8a5eb02cb74372',1,'CppCommon::ArenaMemoryManager']]]
 ];
