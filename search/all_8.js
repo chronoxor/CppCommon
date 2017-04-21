01c36890d58a58ffe7820b62b6fdd79b7d7c151e
Travis CI <travis@travis-ci.org>
2017-04-21 19:10:35 +0000

    Update generated documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['hardlinks',['hardlinks',['../class_cpp_common_1_1_path.html#a3f939fdbc153ff307528746f7722e630',1,'CppCommon::Path']]],
-  ['hasextension',['HasExtension',['../class_cpp_common_1_1_path.html#a42e9f276f0dc905b008acdfc52646598',1,'CppCommon::Path']]],
-  ['hasfilename',['HasFilename',['../class_cpp_common_1_1_path.html#a946b79440a4d3b624e374718cc2e9a11',1,'CppCommon::Path']]],
-  ['hasparent',['HasParent',['../class_cpp_common_1_1_path.html#a726701fb108fdd0614f600d1ef386d2b',1,'CppCommon::Path']]],
-  ['hasrelative',['HasRelative',['../class_cpp_common_1_1_path.html#af4dec9dbc48dbb4d98eecf973395c855',1,'CppCommon::Path']]],
-  ['hasroot',['HasRoot',['../class_cpp_common_1_1_path.html#a73d9b487f36b4fcee7bb4257dd8983cf',1,'CppCommon::Path']]],
-  ['hasstem',['HasStem',['../class_cpp_common_1_1_path.html#a3c3d631cde82fc97f5e7163b400b46ac',1,'CppCommon::Path']]],
+  ['hardlinks',['hardlinks',['../class_cpp_common_1_1_path.html#a1cc9aa58c2f5413a0d20715402810b12',1,'CppCommon::Path']]],
+  ['hasextension',['HasExtension',['../class_cpp_common_1_1_path.html#abc7244f73b849552d82acf6ea4cb00b6',1,'CppCommon::Path']]],
+  ['hasfilename',['HasFilename',['../class_cpp_common_1_1_path.html#a3040322b4d0572c6835480aab6ed5a23',1,'CppCommon::Path']]],
+  ['hasparent',['HasParent',['../class_cpp_common_1_1_path.html#a4f48b5ca5cceedfef943a4e6251018d7',1,'CppCommon::Path']]],
+  ['hasrelative',['HasRelative',['../class_cpp_common_1_1_path.html#a98836d8cc1be481b62cf2d9eb58ddeb3',1,'CppCommon::Path']]],
+  ['hasroot',['HasRoot',['../class_cpp_common_1_1_path.html#a2ed7f1b4d423d97bf78465b4eafdb30f',1,'CppCommon::Path']]],
+  ['hasstem',['HasStem',['../class_cpp_common_1_1_path.html#a2be62830fe45e1e7aaa76abdd62f5a2e',1,'CppCommon::Path']]],
   ['hidden',['HIDDEN',['../namespace_cpp_common.html#af3227fe73424d43d02a24275b3177a6da347e628a8f72626a0611ef842fe9d304',1,'CppCommon']]],
   ['high',['HIGH',['../class_cpp_common_1_1_thread.html#a315326743ff0dcffb87a637fa9173e2fab89de3b4b81c4facfac906edf29aec8c',1,'CppCommon::Thread']]],
   ['highest',['HIGHEST',['../class_cpp_common_1_1_thread.html#a315326743ff0dcffb87a637fa9173e2fad08bf58ccd0f9cf567cf14bf1216369f',1,'CppCommon::Thread']]],
@@ -14,6 +14,7 @@ var searchData=
   ['hour',['hour',['../class_cpp_common_1_1_time.html#aa6bf37549c29fa8edcf1ddb03dab973e',1,'CppCommon::Time']]],
   ['hours',['hours',['../class_cpp_common_1_1_timespan.html#af5aa737510d11db7c38e352cf231b384',1,'CppCommon::Timespan::hours() const noexcept'],['../class_cpp_common_1_1_timespan.html#a81b2ed7a759fb98c8488a0d8c50fa523',1,'CppCommon::Timespan::hours(int64_t hours) noexcept'],['../class_cpp_common_1_1_timestamp.html#ab86196d874346bbae5f31d586448e9c8',1,'CppCommon::Timestamp::hours() const noexcept'],['../class_cpp_common_1_1_timestamp.html#a930864f50feb3c960143490f1542f4dc',1,'CppCommon::Timestamp::hours(int64_t hours) noexcept']]],
   ['hybridallocator',['HybridAllocator',['../namespace_cpp_common.html#a5d18e19ef78602df37c3c19fc53614b6',1,'CppCommon']]],
-  ['hybridmemorymanager',['HybridMemoryManager',['../class_cpp_common_1_1_hybrid_memory_manager.html',1,'CppCommon::HybridMemoryManager&lt; TAuxMemoryManager, alignment &gt;'],['../class_cpp_common_1_1_hybrid_memory_manager.html#a020792b8ed429d3bb8204d349999e1d8',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(TAuxMemoryManager &amp;auxiliary, size_t capacity)'],['../class_cpp_common_1_1_hybrid_memory_manager.html#aa444b0e37733bcaada7a349eb420a2a9',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(TAuxMemoryManager &amp;auxiliary, uint8_t *buffer, size_t size)'],['../class_cpp_common_1_1_hybrid_memory_manager.html#ad633e1e5c0e9e931658ac91e1b00a8c5',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(const HybridMemoryManager &amp;) noexcept=delete'],['../class_cpp_common_1_1_hybrid_memory_manager.html#ac0a2ba2f64990d9f22a2687fc78fee00',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(HybridMemoryManager &amp;&amp;) noexcept=default']]],
+  ['hybridmemorymanager',['HybridMemoryManager',['../class_cpp_common_1_1_hybrid_memory_manager.html',1,'CppCommon']]],
+  ['hybridmemorymanager',['HybridMemoryManager',['../class_cpp_common_1_1_hybrid_memory_manager.html#a020792b8ed429d3bb8204d349999e1d8',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(TAuxMemoryManager &amp;auxiliary, size_t capacity)'],['../class_cpp_common_1_1_hybrid_memory_manager.html#aa444b0e37733bcaada7a349eb420a2a9',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(TAuxMemoryManager &amp;auxiliary, uint8_t *buffer, size_t size)'],['../class_cpp_common_1_1_hybrid_memory_manager.html#ad633e1e5c0e9e931658ac91e1b00a8c5',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(const HybridMemoryManager &amp;) noexcept=delete'],['../class_cpp_common_1_1_hybrid_memory_manager.html#ac0a2ba2f64990d9f22a2687fc78fee00',1,'CppCommon::HybridMemoryManager::HybridMemoryManager(HybridMemoryManager &amp;&amp;) noexcept=default']]],
   ['hyperthreading',['HyperThreading',['../class_cpp_common_1_1_c_p_u.html#a27cacc71999419f0bc0c720d6ec60520',1,'CppCommon::CPU']]]
 ];
