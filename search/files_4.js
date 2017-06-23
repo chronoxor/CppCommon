03258fbfe1c590c85f6c6f203d6bffbd0d6a5eda
Travis CI <travis@travis-ci.org>
2017-06-23 18:36:56 +0000

    Update generated documentation

--- search/files_4.js
+++ search/files_4.js
@@ -8,11 +8,8 @@ var searchData=
   ['event_5fauto_5freset_2eh',['event_auto_reset.h',['../event__auto__reset_8h.html',1,'']]],
   ['event_5fmanual_5freset_2ecpp',['event_manual_reset.cpp',['../event__manual__reset_8cpp.html',1,'']]],
   ['event_5fmanual_5freset_2eh',['event_manual_reset.h',['../event__manual__reset_8h.html',1,'']]],
-  ['exceptions_2ecpp',['exceptions.cpp',['../errors_2exceptions_8cpp.html',1,'']]],
-  ['exceptions_2ecpp',['exceptions.cpp',['../filesystem_2exceptions_8cpp.html',1,'']]],
-  ['exceptions_2eh',['exceptions.h',['../system_2exceptions_8h.html',1,'']]],
-  ['exceptions_2eh',['exceptions.h',['../errors_2exceptions_8h.html',1,'']]],
-  ['exceptions_2eh',['exceptions.h',['../filesystem_2exceptions_8h.html',1,'']]],
+  ['exceptions_2ecpp',['exceptions.cpp',['../errors_2exceptions_8cpp.html',1,'(Global Namespace)'],['../filesystem_2exceptions_8cpp.html',1,'(Global Namespace)']]],
+  ['exceptions_2eh',['exceptions.h',['../errors_2exceptions_8h.html',1,'(Global Namespace)'],['../filesystem_2exceptions_8h.html',1,'(Global Namespace)'],['../system_2exceptions_8h.html',1,'(Global Namespace)']]],
   ['exceptions_5fhandler_2ecpp',['exceptions_handler.cpp',['../exceptions__handler_8cpp.html',1,'']]],
   ['exceptions_5fhandler_2eh',['exceptions_handler.h',['../exceptions__handler_8h.html',1,'']]]
 ];
