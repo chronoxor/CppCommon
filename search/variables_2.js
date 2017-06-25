7ba0b510fba43aa0822143fd1cb4eab41ba4f5e9
Travis CI <travis@travis-ci.org>
2017-06-25 19:06:01 +0000

    Update generated documentation

--- search/variables_2.js
+++ search/variables_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['balance',['balance',['../struct_cpp_common_1_1_bin_tree_a_v_l_1_1_node.html#acbe35eac918cdf04efa9b6460ab3be08',1,'CppCommon::BinTreeAVL::Node']]]
+  ['balance',['balance',['../struct_cpp_common_1_1_bin_tree_a_v_l_1_1_node.html#af854bc5eb75c1666e68accc8bcd623e0',1,'CppCommon::BinTreeAVL::Node']]]
 ];
