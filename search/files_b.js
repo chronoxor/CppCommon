22a263df834519010d1182b59b981fcda9190ee3
Travis CI <travis@travis-ci.org>
2017-07-11 19:16:24 +0000

    Update generated documentation

--- search/files_b.js
+++ search/files_b.js
@@ -1,8 +1,5 @@
 var searchData=
 [
-  ['reader_2ecpp',['reader.cpp',['../reader_8cpp.html',1,'']]],
-  ['reader_2eh',['reader.h',['../reader_8h.html',1,'']]],
-  ['resource_2eh',['resource.h',['../resource_8h.html',1,'']]],
-  ['rw_5flock_2ecpp',['rw_lock.cpp',['../rw__lock_8cpp.html',1,'']]],
-  ['rw_5flock_2eh',['rw_lock.h',['../rw__lock_8h.html',1,'']]]
+  ['queue_2eh',['queue.h',['../queue_8h.html',1,'']]],
+  ['queue_2einl',['queue.inl',['../queue_8inl.html',1,'']]]
 ];
