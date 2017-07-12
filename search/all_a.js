1dc5bb313cdf752554ce6fcdbeb7b8ec4df94052
AppVeyor <team@appveyor.com>
2017-07-12 14:23:19 -0700

    Update generated documentation

--- search/all_a.js
+++ search/all_a.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['key_5fequal',['key_equal',['../class_cpp_common_1_1_hash_map.html#adf1952418b5ccddec6be77295939fdd9',1,'CppCommon::HashMap::key_equal()'],['../class_cpp_common_1_1_hash_map.html#a6c96a08dd329ea0aab0ee9469ed9b2f9',1,'CppCommon::HashMap::key_equal(const TKey &amp;key1, const TKey &amp;equal2) const noexcept']]],
-  ['key_5fhash',['key_hash',['../class_cpp_common_1_1_hash_map.html#ad62bc3466377b903ac52f1efc7a97b88',1,'CppCommon::HashMap']]],
+  ['key_5fequal',['key_equal',['../class_cpp_common_1_1_hash_map.html#adf1952418b5ccddec6be77295939fdd9',1,'CppCommon::HashMap::key_equal()'],['../class_cpp_common_1_1_hash_map.html#add21043e23e4e76f81d35c2a8d6a4503',1,'CppCommon::HashMap::key_equal(const TKey &amp;key1, const TKey &amp;equal2) const  noexcept']]],
+  ['key_5fhash',['key_hash',['../class_cpp_common_1_1_hash_map.html#a52c05f5936dce874d4a67bddee28116e',1,'CppCommon::HashMap']]],
   ['key_5ftype',['key_type',['../class_cpp_common_1_1_hash_map.html#ad9ab67cde3cf5e5cc37e0f297cf5f264',1,'CppCommon::HashMap']]],
   ['kill',['Kill',['../class_cpp_common_1_1_process.html#a4b95a88b0c70b6a4ea1c4f567a176df9',1,'CppCommon::Process']]]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['key_5fequal',['key_equal',['../class_cpp_common_1_1_hash_map.html#a6c96a08dd329ea0aab0ee9469ed9b2f9',1,'CppCommon::HashMap']]],
-  ['key_5fhash',['key_hash',['../class_cpp_common_1_1_hash_map.html#ad62bc3466377b903ac52f1efc7a97b88',1,'CppCommon::HashMap']]],
+  ['key_5fequal',['key_equal',['../class_cpp_common_1_1_hash_map.html#add21043e23e4e76f81d35c2a8d6a4503',1,'CppCommon::HashMap']]],
+  ['key_5fhash',['key_hash',['../class_cpp_common_1_1_hash_map.html#a52c05f5936dce874d4a67bddee28116e',1,'CppCommon::HashMap']]],
   ['kill',['Kill',['../class_cpp_common_1_1_process.html#a4b95a88b0c70b6a4ea1c4f567a176df9',1,'CppCommon::Process']]]
 ];
