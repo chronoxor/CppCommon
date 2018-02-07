0b07f933f7dff1ffe729f12ea40189188debde28
Travis CI <travis@travis-ci.org>
2018-02-07 11:13:26 +0000

    Update generated documentation

--- search/enumvalues_3.js
+++ search/enumvalues_3.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['darkgrey',['DARKGREY',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4a571a40100f7bd18fda2191490111e6cf',1,'CppCommon']]],
-  ['destroy',['Destroy',['../class_cpp_common_1_1_function_3_01_r_07_args_8_8_8_08_00_01_capacity_01_4.html#ad22513ef7f10cb18bfd1c6797bfccfc2a0e181f89f47654b86f3beb42f5cc08b8',1,'CppCommon::Function&lt; R(Args...), Capacity &gt;']]],
   ['directory',['DIRECTORY',['../namespace_cpp_common.html#a3566e27bae7f971c3c1547098a74a12fa28766b4be065d0c806c2e9c9d914af48',1,'CppCommon']]]
 ];
