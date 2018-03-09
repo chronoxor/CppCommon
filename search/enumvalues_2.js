3cef93e661dd5ea635b30a991631b792b74cacec
Travis CI <travis@travis-ci.org>
2018-03-09 16:06:05 +0000

    Update generated documentation

--- search/enumvalues_2.js
+++ search/enumvalues_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['character',['CHARACTER',['../namespace_cpp_common.html#a3566e27bae7f971c3c1547098a74a12fa3c94fe830ac18aa182887ab6ac05b596',1,'CppCommon']]],
+  ['clone',['Clone',['../class_cpp_common_1_1_function_3_01_r_07_args_8_8_8_08_00_01_capacity_01_4.html#ad22513ef7f10cb18bfd1c6797bfccfc2aff24590464659ee8cdec688128c35f89',1,'CppCommon::Function&lt; R(Args...), Capacity &gt;']]],
   ['cyan',['CYAN',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4a344dd8cd533280795b9db82ef0c92749',1,'CppCommon']]]
 ];
