3c81678f19d04c0919c4a1533918db4272fa3ec0
Ivan Shynkarenka <chronoxor@gmail.com>
2018-03-09 15:44:01 +0300

    Fix <codecvt> deprecations in Visual Studio 2017

--- source/string/encoding.cpp
+++ source/string/encoding.cpp
@@ -11,6 +11,11 @@
 #include <codecvt>
 #include <locale>
 
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable: 4996) // C4996: 'function': was declared deprecated
+#endif
+
 namespace CppCommon {
 
 std::string Encoding::ToUTF8(const std::wstring& wstr)
@@ -129,3 +134,8 @@ std::u16string Encoding::UTF32toUTF16(const std::u32string& str)
 }
 
 } // namespace CppCommon
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
+
