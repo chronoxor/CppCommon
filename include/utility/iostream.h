9cd10518be4b1304b69adb371dccaa673e4644d6
Ivan Shynkarenka <chronoxor@gmail.com>
2018-08-05 01:54:55 +0300

    update

--- include/utility/iostream.h
+++ include/utility/iostream.h
@@ -28,6 +28,21 @@ private:
     char _ch;
 };
 
+//! Write wide character stream manipulator
+class WriteWChar
+{
+public:
+    explicit WriteWChar(wchar_t ch) : _ch(ch) {}
+
+    friend std::ostream& operator<<(std::ostream& stream, const WriteWChar& writer)
+    { return stream << '\'' << writer._ch << '\''; }
+    friend std::wostream& operator<<(std::wostream& stream, const WriteWChar& writer)
+    { return stream << '\'' << writer._ch << '\''; }
+
+private:
+    wchar_t _ch;
+};
+
 //! Write string stream manipulator
 class WriteString
 {
@@ -53,6 +68,34 @@ public:
 
 private:
     const char* _str;
+    size_t _size;
+};
+
+//! Write wide string stream manipulator
+class WriteWString
+{
+public:
+    template <size_t N>
+    explicit WriteWString(const wchar_t (&str)[N]) : _str(str), _size(N) {}
+
+    friend std::ostream& operator<<(std::ostream& stream, const WriteWString& writer)
+    {
+        stream << '"';
+        for (size_t i = 0; i < writer._size; ++i)
+            stream << writer._str[i];
+        stream << '"';
+        return stream;
+    }
+    friend std::wostream& operator<<(std::wostream& stream, const WriteWString& writer)
+    {
+        stream << '"';
+        stream.write(writer._str, writer._size);
+        stream << '"';
+        return stream;
+    }
+
+private:
+    const wchar_t* _str;
     size_t _size;
 };
 
