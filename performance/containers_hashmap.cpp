988f5180e1590cc7da4548de9650e410dce43f22
Ivan Shynkarenka <chronoxor@gmail.com>
2018-09-30 00:37:39 +0300

    Add new hash table tests: https://github.com/Tessil/hopscotch-map https://github.com/Tessil/ordered-map https://github.com/Tessil/robin-map https://github.com/Tessil/sparse-map

--- performance/containers_hashmap.cpp
+++ performance/containers_hashmap.cpp
@@ -16,12 +16,18 @@
 #pragma GCC diagnostic ignored "-Wshadow"
 #elif defined(_MSC_VER)
 #pragma warning(push)
+#pragma warning(disable: 4127) // C4127: conditional expression is constant
 #pragma warning(disable: 4244) // C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
 #pragma warning(disable: 4310) // C4310: cast truncates constant value
 #pragma warning(disable: 4458) // C4458: declaration of 'identifier' hides class member
 #endif
 #include "flat_hash_map/flat_hash_map.hpp"
 #include "flat_hash_map/bytell_hash_map.hpp"
+#include "hopscotch-map/bhopscotch_map.h"
+#include "hopscotch-map/hopscotch_map.h"
+#include "ordered-map/ordered_map.h"
+#include "robin-map/robin_map.h"
+#include "sparse-map/sparse_map.h"
 #if defined(__GNUC__)
 #pragma GCC diagnostic pop
 #elif defined(_MSC_VER)
@@ -35,6 +41,11 @@ typedef std::unordered_map<int, int> UnorderedMap;
 typedef CppCommon::HashMap<int, int> HashMap;
 typedef ska::flat_hash_map<int, int> FlatHash;
 typedef ska::bytell_hash_map<int, int> BytellHash;
+typedef tsl::bhopscotch_map<int, int> BHopscotchHash;
+typedef tsl::hopscotch_map<int, int> HopscotchHash;
+typedef tsl::ordered_map<int, int> OrderedHash;
+typedef tsl::robin_map<int, int> RobinHash;
+typedef tsl::sparse_map<int, int> SparseHash;
 
 template <class T>
 class InsertFixture : public virtual CppBenchmark::Fixture
@@ -120,6 +131,51 @@ BENCHMARK_FIXTURE(InsertFixture<BytellHash>, "Insert: BytellHash")
     context.metrics().AddOperations(items - 1);
 }
 
+BENCHMARK_FIXTURE(InsertFixture<BHopscotchHash>, "Insert: BHopscotchHash")
+{
+    for (auto& value : this->values)
+        this->map.emplace(value, value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+}
+
+BENCHMARK_FIXTURE(InsertFixture<HopscotchHash>, "Insert: HopscotchHash")
+{
+    for (auto& value : this->values)
+        this->map.emplace(value, value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+}
+
+BENCHMARK_FIXTURE(InsertFixture<OrderedHash>, "Insert: OrderedHash")
+{
+    for (auto& value : this->values)
+        this->map.emplace(value, value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+}
+
+BENCHMARK_FIXTURE(InsertFixture<RobinHash>, "Insert: RobinHash")
+{
+    for (auto& value : this->values)
+        this->map.emplace(value, value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+}
+
+BENCHMARK_FIXTURE(InsertFixture<SparseHash>, "Insert: SparseHash")
+{
+    for (auto& value : this->values)
+        this->map.emplace(value, value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+}
+
 BENCHMARK_FIXTURE(FindFixture<Map>, "Find: std::map")
 {
     uint64_t crc = 0;
@@ -180,6 +236,66 @@ BENCHMARK_FIXTURE(FindFixture<BytellHash>, "Find: BytellHash")
     context.metrics().SetCustom("CRC", crc);
 }
 
+BENCHMARK_FIXTURE(FindFixture<BHopscotchHash>, "Find: BHopscotchHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.find(value)->second;
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<HopscotchHash>, "Find: HopscotchHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.find(value)->second;
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<OrderedHash>, "Find: OrderedHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.find(value)->second;
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<RobinHash>, "Find: RobinHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.find(value)->second;
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<SparseHash>, "Find: SparseHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.find(value)->second;
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
 BENCHMARK_FIXTURE(FindFixture<Map>, "Remove: std::map")
 {
     uint64_t crc = 0;
@@ -229,6 +345,66 @@ BENCHMARK_FIXTURE(FindFixture<FlatHash>, "Remove: FlatHash")
 }
 
 BENCHMARK_FIXTURE(FindFixture<BytellHash>, "Remove: BytellHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.erase(value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<BHopscotchHash>, "Remove: BHopscotchHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.erase(value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<HopscotchHash>, "Remove: HopscotchHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.erase(value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<OrderedHash>, "Remove: OrderedHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.erase(value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<RobinHash>, "Remove: RobinHash")
+{
+    uint64_t crc = 0;
+
+    for (auto& value : this->values)
+        crc += this->map.erase(value);
+
+    // Update benchmark metrics
+    context.metrics().AddOperations(items - 1);
+    context.metrics().SetCustom("CRC", crc);
+}
+
+BENCHMARK_FIXTURE(FindFixture<SparseHash>, "Remove: SparseHash")
 {
     uint64_t crc = 0;
 
