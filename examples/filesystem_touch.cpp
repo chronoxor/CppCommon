f3690b662dbbd6103b659efbe3268db563beb6ea
Ivan Shynkarenka <chronoxor@gmail.com>
2019-03-21 16:21:31 +0300

    Add filesystem touch example

---
+++ examples/filesystem_touch.cpp
@@ -0,0 +1,24 @@
+/*!
+    \file filesystem_touch.cpp
+    \brief Filesystem touch example
+    \author Ivan Shynkarenka
+    \date 21.03.2019
+    \copyright MIT License
+*/
+
+#include "filesystem/path.h"
+#include "time/timestamp.h"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "Touch path is required!" << std::endl;
+        return -1;
+    }
+
+    CppCommon::Path::SetModified(argv[1], CppCommon::UtcTimestamp());
+    return 0;
+}
