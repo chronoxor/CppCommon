83ea887e1b7fa4379a4e7e9bb3f85d7088c92660
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-04 23:03:46 +0300

    build

--- source/string/password.cpp
+++ source/string/password.cpp
@@ -8,10 +8,7 @@
 
 #include "string/password.h"
 
-#if defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
-#define __USE_MISC
-#include <string.h>
-#elif defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
 
@@ -19,15 +16,11 @@ namespace CppCommon {
 
 void ZeroPasswordMemory(void* buffer, size_t size)
 {
-#if defined(__APPLE__)
-    volatile char* ptr = buffer;
-    while (size--) *ptr++ = 0;
-#elif defined(unix) || defined(__unix) || defined(__unix__)
-    explicit_bzero(buffer, size);
-#elif defined(_WIN32) || defined(_WIN64)
+#if defined(_WIN32) || defined(_WIN64)
     SecureZeroMemory(buffer, size);
 #else
-    #error Unsupported platform
+    volatile char* ptr = (volatile char*)buffer;
+    while (size--) *ptr++ = 0;
 #endif
 }
 
