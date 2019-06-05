6206721b0a4f3091c6bed9e3cd14e2917a815837
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-05 11:26:56 +0300

    update

--- source/string/password.cpp
+++ source/string/password.cpp
@@ -8,7 +8,11 @@
 
 #include "string/password.h"
 
-#if defined(_WIN32) || defined(_WIN64)
+#if defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__)
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdlib.h>
+#include <string.h>
+#elif defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
 
@@ -16,7 +20,9 @@ namespace CppCommon {
 
 void ZeroPasswordMemory(void* buffer, size_t size)
 {
-#if defined(_WIN32) || defined(_WIN64)
+#ifdef __STDC_LIB_EXT1__
+    memset_s(buffer, size, 0, size);
+#elif defined(_WIN32) || defined(_WIN64)
     SecureZeroMemory(buffer, size);
 #else
     volatile char* ptr = (volatile char*)buffer;
