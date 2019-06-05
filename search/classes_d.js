edd6dc107bdd61512ca75ad429b613e4505ca86f
Travis CI <travis@travis-ci.org>
2019-06-05 18:30:17 +0000

    Update generated documentation

--- search/classes_d.js
+++ search/classes_d.js
@@ -3,7 +3,9 @@ var searchData=
   ['rdtstimestamp',['RdtsTimestamp',['../class_cpp_common_1_1_rdts_timestamp.html',1,'CppCommon']]],
   ['reader',['Reader',['../class_cpp_common_1_1_reader.html',1,'CppCommon']]],
   ['readlocker',['ReadLocker',['../class_cpp_common_1_1_read_locker.html',1,'CppCommon']]],
-  ['rebind',['rebind',['../struct_cpp_common_1_1_password_allocator_1_1rebind.html',1,'CppCommon::PasswordAllocator&lt; T &gt;::rebind&lt; U &gt;'],['../struct_cpp_common_1_1_allocator_1_1rebind.html',1,'CppCommon::Allocator&lt; T, TMemoryManager, nothrow &gt;::rebind&lt; TOther &gt;'],['../struct_cpp_common_1_1_allocator_3_01void_00_01_t_memory_manager_00_01nothrow_01_4_1_1rebind.html',1,'CppCommon::Allocator&lt; void, TMemoryManager, nothrow &gt;::rebind&lt; TOther &gt;']]],
+  ['rebind',['rebind',['../struct_cpp_common_1_1_password_allocator_1_1rebind.html',1,'CppCommon::PasswordAllocator']]],
+  ['rebind',['rebind',['../struct_cpp_common_1_1_allocator_1_1rebind.html',1,'CppCommon::Allocator']]],
+  ['rebind',['rebind',['../struct_cpp_common_1_1_allocator_3_01void_00_01_t_memory_manager_00_01nothrow_01_4_1_1rebind.html',1,'CppCommon::Allocator&lt; void, TMemoryManager, nothrow &gt;']]],
   ['runtimeexception',['RuntimeException',['../class_cpp_common_1_1_runtime_exception.html',1,'CppCommon']]],
   ['rwlock',['RWLock',['../class_cpp_common_1_1_r_w_lock.html',1,'CppCommon']]]
 ];
