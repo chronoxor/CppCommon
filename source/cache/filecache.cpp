9f966c1d19e3eff84e1221a0d386702acb58c5ca
Ivan Shynkarenka <chronoxor@gmail.com>
2019-06-20 17:57:50 +0300

    update

--- source/cache/filecache.cpp
+++ source/cache/filecache.cpp
@@ -135,7 +135,7 @@ bool FileCache::insert_path_internal(const CppCommon::Path& path, const std::str
         // Iterate through all directory entries
         for (const auto& item : CppCommon::Directory(path))
         {
-            const CppCommon::Path entry = item.IsSymlink() ? Symlink(entry).target() : item;
+            const CppCommon::Path entry = item.IsSymlink() ? Symlink(item).target() : item;
             const std::string key = key_prefix + CppCommon::Encoding::URLDecode(entry.filename().string());
 
             if (entry.IsDirectory())
