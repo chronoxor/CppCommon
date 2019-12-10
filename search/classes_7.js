8c36ac5b4851ba46ee609c19195f6997785c2d07
Travis CI <travis@travis-ci.org>
2019-12-10 22:48:19 +0000

    Update generated documentation

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,10 +1,4 @@
 var searchData=
 [
-  ['is_5farithmetic_3c_20cppcommon_3a_3auint128_5ft_20_3e',['is_arithmetic&lt; CppCommon::uint128_t &gt;',['../structstd_1_1is__arithmetic_3_01_cpp_common_1_1uint128__t_01_4.html',1,'std']]],
-  ['is_5farithmetic_3c_20cppcommon_3a_3auint256_5ft_20_3e',['is_arithmetic&lt; CppCommon::uint256_t &gt;',['../structstd_1_1is__arithmetic_3_01_cpp_common_1_1uint256__t_01_4.html',1,'std']]],
-  ['is_5fintegral_3c_20cppcommon_3a_3auint128_5ft_20_3e',['is_integral&lt; CppCommon::uint128_t &gt;',['../structstd_1_1is__integral_3_01_cpp_common_1_1uint128__t_01_4.html',1,'std']]],
-  ['is_5fintegral_3c_20cppcommon_3a_3auint256_5ft_20_3e',['is_integral&lt; CppCommon::uint256_t &gt;',['../structstd_1_1is__integral_3_01_cpp_common_1_1uint256__t_01_4.html',1,'std']]],
-  ['is_5funsigned_3c_20cppcommon_3a_3auint128_5ft_20_3e',['is_unsigned&lt; CppCommon::uint128_t &gt;',['../structstd_1_1is__unsigned_3_01_cpp_common_1_1uint128__t_01_4.html',1,'std']]],
-  ['is_5funsigned_3c_20cppcommon_3a_3auint256_5ft_20_3e',['is_unsigned&lt; CppCommon::uint256_t &gt;',['../structstd_1_1is__unsigned_3_01_cpp_common_1_1uint256__t_01_4.html',1,'std']]],
   ['isenumflags',['IsEnumFlags',['../struct_cpp_common_1_1_is_enum_flags.html',1,'CppCommon']]]
 ];
