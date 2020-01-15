97c9bb3e7469c3db24e484499131082c34343049
Travis CI <travis@travis-ci.org>
2020-01-15 15:59:25 +0000

    Update generated documentation

--- search/files_3.js
+++ search/files_3.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['directory_2ecpp_1207',['directory.cpp',['../directory_8cpp.html',1,'']]],
-  ['directory_2eh_1208',['directory.h',['../directory_8h.html',1,'']]],
-  ['directory_2einl_1209',['directory.inl',['../directory_8inl.html',1,'']]],
-  ['directory_5fiterator_2ecpp_1210',['directory_iterator.cpp',['../directory__iterator_8cpp.html',1,'']]],
-  ['directory_5fiterator_2eh_1211',['directory_iterator.h',['../directory__iterator_8h.html',1,'']]],
-  ['directory_5fiterator_2einl_1212',['directory_iterator.inl',['../directory__iterator_8inl.html',1,'']]],
-  ['dll_2ecpp_1213',['dll.cpp',['../dll_8cpp.html',1,'']]],
-  ['dll_2eh_1214',['dll.h',['../dll_8h.html',1,'']]],
-  ['dll_2einl_1215',['dll.inl',['../dll_8inl.html',1,'']]]
+  ['directory_2ecpp',['directory.cpp',['../directory_8cpp.html',1,'']]],
+  ['directory_2eh',['directory.h',['../directory_8h.html',1,'']]],
+  ['directory_2einl',['directory.inl',['../directory_8inl.html',1,'']]],
+  ['directory_5fiterator_2ecpp',['directory_iterator.cpp',['../directory__iterator_8cpp.html',1,'']]],
+  ['directory_5fiterator_2eh',['directory_iterator.h',['../directory__iterator_8h.html',1,'']]],
+  ['directory_5fiterator_2einl',['directory_iterator.inl',['../directory__iterator_8inl.html',1,'']]],
+  ['dll_2ecpp',['dll.cpp',['../dll_8cpp.html',1,'']]],
+  ['dll_2eh',['dll.h',['../dll_8h.html',1,'']]],
+  ['dll_2einl',['dll.inl',['../dll_8inl.html',1,'']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['address_2065',['address',['../struct_cpp_common_1_1_stack_trace_1_1_frame.html#ada2332e0e3540884870f01f7fe1216b6',1,'CppCommon::StackTrace::Frame']]],
-  ['available_2066',['available',['../struct_cpp_common_1_1_space_info.html#ad97a76d67cd5485d8442936ee2347960',1,'CppCommon::SpaceInfo']]]
+  ['address',['address',['../struct_cpp_common_1_1_stack_trace_1_1_frame.html#ada2332e0e3540884870f01f7fe1216b6',1,'CppCommon::StackTrace::Frame']]],
+  ['available',['available',['../struct_cpp_common_1_1_space_info.html#ad97a76d67cd5485d8442936ee2347960',1,'CppCommon::SpaceInfo']]]
 ];
