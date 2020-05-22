e56beba08c1623ae888859932b503ffdf21b86ae
Travis CI <travis@travis-ci.org>
2020-05-22 18:57:12 +0000

    Update generated documentation

--- search/classes_5.js
+++ search/classes_5.js
@@ -1,14 +1,14 @@
 var searchData=
 [
-  ['file_1056',['File',['../class_cpp_common_1_1_file.html',1,'CppCommon']]],
-  ['filecache_1057',['FileCache',['../class_cpp_common_1_1_file_cache.html',1,'CppCommon']]],
-  ['filelock_1058',['FileLock',['../class_cpp_common_1_1_file_lock.html',1,'CppCommon']]],
-  ['filesystemexception_1059',['FileSystemException',['../class_cpp_common_1_1_file_system_exception.html',1,'CppCommon']]],
-  ['flags_1060',['Flags',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
-  ['flags_3c_20fileattributes_20_3e_1061',['Flags&lt; FileAttributes &gt;',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
-  ['flags_3c_20filepermissions_20_3e_1062',['Flags&lt; FilePermissions &gt;',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
-  ['flatmap_1063',['FlatMap',['../class_cpp_common_1_1_flat_map.html',1,'CppCommon']]],
-  ['frame_1064',['Frame',['../struct_cpp_common_1_1_stack_trace_1_1_frame.html',1,'CppCommon::StackTrace']]],
-  ['function_1065',['Function',['../class_cpp_common_1_1_function.html',1,'CppCommon']]],
-  ['function_3c_20r_28args_2e_2e_2e_29_2c_20capacity_20_3e_1066',['Function&lt; R(Args...), Capacity &gt;',['../class_cpp_common_1_1_function_3_01_r_07_args_8_8_8_08_00_01_capacity_01_4.html',1,'CppCommon']]]
+  ['file',['File',['../class_cpp_common_1_1_file.html',1,'CppCommon']]],
+  ['filecache',['FileCache',['../class_cpp_common_1_1_file_cache.html',1,'CppCommon']]],
+  ['filelock',['FileLock',['../class_cpp_common_1_1_file_lock.html',1,'CppCommon']]],
+  ['filesystemexception',['FileSystemException',['../class_cpp_common_1_1_file_system_exception.html',1,'CppCommon']]],
+  ['flags',['Flags',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
+  ['flags_3c_20fileattributes_20_3e',['Flags&lt; FileAttributes &gt;',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
+  ['flags_3c_20filepermissions_20_3e',['Flags&lt; FilePermissions &gt;',['../class_cpp_common_1_1_flags.html',1,'CppCommon']]],
+  ['flatmap',['FlatMap',['../class_cpp_common_1_1_flat_map.html',1,'CppCommon']]],
+  ['frame',['Frame',['../struct_cpp_common_1_1_stack_trace_1_1_frame.html',1,'CppCommon::StackTrace']]],
+  ['function',['Function',['../class_cpp_common_1_1_function.html',1,'CppCommon']]],
+  ['function_3c_20r_28args_2e_2e_2e_29_2c_20capacity_20_3e',['Function&lt; R(Args...), Capacity &gt;',['../class_cpp_common_1_1_function_3_01_r_07_args_8_8_8_08_00_01_capacity_01_4.html',1,'CppCommon']]]
 ];
--- search/files_12.js
+++ search/files_12.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['wait_5fbatcher_2eh_1376',['wait_batcher.h',['../wait__batcher_8h.html',1,'']]],
-  ['wait_5fbatcher_2einl_1377',['wait_batcher.inl',['../wait__batcher_8inl.html',1,'']]],
-  ['wait_5fqueue_2eh_1378',['wait_queue.h',['../wait__queue_8h.html',1,'']]],
-  ['wait_5fqueue_2einl_1379',['wait_queue.inl',['../wait__queue_8inl.html',1,'']]],
-  ['wait_5fring_2eh_1380',['wait_ring.h',['../wait__ring_8h.html',1,'']]],
-  ['wait_5fring_2einl_1381',['wait_ring.inl',['../wait__ring_8inl.html',1,'']]],
-  ['writer_2ecpp_1382',['writer.cpp',['../writer_8cpp.html',1,'']]],
-  ['writer_2eh_1383',['writer.h',['../writer_8h.html',1,'']]]
+  ['wait_5fbatcher_2eh',['wait_batcher.h',['../wait__batcher_8h.html',1,'']]],
+  ['wait_5fbatcher_2einl',['wait_batcher.inl',['../wait__batcher_8inl.html',1,'']]],
+  ['wait_5fqueue_2eh',['wait_queue.h',['../wait__queue_8h.html',1,'']]],
+  ['wait_5fqueue_2einl',['wait_queue.inl',['../wait__queue_8inl.html',1,'']]],
+  ['wait_5fring_2eh',['wait_ring.h',['../wait__ring_8h.html',1,'']]],
+  ['wait_5fring_2einl',['wait_ring.inl',['../wait__ring_8inl.html',1,'']]],
+  ['writer_2ecpp',['writer.cpp',['../writer_8cpp.html',1,'']]],
+  ['writer_2eh',['writer.h',['../writer_8h.html',1,'']]]
 ];
