b4257cd7c7a8e6b448f252a47ec260be4b4bcc75
Travis CI <travis@travis-ci.org>
2020-09-17 13:15:28 +0000

    Update generated documentation

--- search/enumvalues_10.js
+++ search/enumvalues_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['white_2172',['WHITE',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4ab5bf627e448384cf3a4c35121ca6008d',1,'CppCommon']]]
+  ['white_2170',['WHITE',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4ab5bf627e448384cf3a4c35121ca6008d',1,'CppCommon']]]
 ];
