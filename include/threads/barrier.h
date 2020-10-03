540853aa052b0d0bb951875cbd648e57f2115e76
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-03 04:34:17 +0300

    update

--- include/threads/barrier.h
+++ include/threads/barrier.h
@@ -56,7 +56,7 @@ private:
     Impl& impl() noexcept { return reinterpret_cast<Impl&>(_storage); }
     const Impl& impl() const noexcept { return reinterpret_cast<Impl const&>(_storage); }
 
-    static const size_t StorageSize = 96;
+    static const size_t StorageSize = 128;
     static const size_t StorageAlign = 8;
     std::aligned_storage<StorageSize, StorageAlign>::type _storage;
 };
