f92c029b10d8f59a5363b79fab2a73897052670a
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-07 20:12:56 +0300

    build

--- modules/fmt.cmake
+++ modules/fmt.cmake
@@ -3,7 +3,11 @@ if(NOT TARGET fmt)
   # Module library
   file(GLOB SOURCE_FILES "fmt/src/*.cc")
   add_library(fmt ${SOURCE_FILES})
-  set_target_properties(fmt PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  if(MSVC)
+    set_target_properties(fmt PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  else()
+    set_target_properties(zlib PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-shadow")
+  endif()
   target_include_directories(fmt PUBLIC "fmt/include")
   target_link_libraries(fmt)
 
