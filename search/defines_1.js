83ce6c1862cb140dad1a54851aa4db5f3e386686
Travis CI <travis@travis-ci.org>
2020-10-29 16:14:04 +0000

    Update generated documentation

--- search/defines_1.js
+++ search/defines_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['api_2221',['API',['../dll_8h.html#ad8ce4efaa307683d3d763b37b4711c53',1,'dll.h']]]
+  ['api_2219',['API',['../dll_8h.html#ad8ce4efaa307683d3d763b37b4711c53',1,'dll.h']]]
 ];
