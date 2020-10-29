736363bf43891e2fc45bda941af95335aa74a5bb
Travis CI <travis@travis-ci.org>
2020-10-29 16:23:39 +0000

    Update generated documentation

--- search/classes_11.js
+++ search/classes_11.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['validatealignedstorage_1165',['ValidateAlignedStorage',['../class_cpp_common_1_1_validate_aligned_storage.html',1,'CppCommon']]],
-  ['validatealignedstorage_3c_20storagesize_2c_20storagealign_2c_20implsize_2c_20implalign_2c_20typename_20std_3a_3aenable_5fif_3c_28storagesize_20_3e_3dimplsize_29_20_26_26_28_28storagealign_20_25_20implalign_29_3d_3d0_29_3e_3a_3atype_20_3e_1166',['ValidateAlignedStorage&lt; StorageSize, StorageAlign, ImplSize, ImplAlign, typename std::enable_if&lt;(StorageSize &gt;=ImplSize) &amp;&amp;((StorageAlign % ImplAlign)==0)&gt;::type &gt;',['../class_cpp_common_1_1_validate_aligned_storage_3_01_storage_size_00_01_storage_align_00_01_impl_sc267945a60e67b809372d72010a482c3.html',1,'CppCommon']]]
+  ['validatealignedstorage_3c_20implsize_2c_20implalign_2c_20storagesize_2c_20storagealign_2c_20typename_20std_3a_3aenable_5fif_3c_28storagesize_20_3e_3dimplsize_29_20_26_26_28_28storagealign_20_25_20implalign_29_3d_3d0_29_3e_3a_3atype_20_3e_1166',['ValidateAlignedStorage&lt; ImplSize, ImplAlign, StorageSize, StorageAlign, typename std::enable_if&lt;(StorageSize &gt;=ImplSize) &amp;&amp;((StorageAlign % ImplAlign)==0)&gt;::type &gt;',['../class_cpp_common_1_1_validate_aligned_storage_3_01_impl_size_00_01_impl_align_00_01_storage_sizeea65f23c45a571bf7da444998cdf1c54.html',1,'CppCommon']]]
 ];
