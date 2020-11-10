c8760a60bfa6fca8f703baad4f9bcd2668dd37c1
Travis CI <travis@travis-ci.org>
2020-11-10 08:52:56 +0000

    Update generated documentation

--- search/enumvalues_11.js
+++ search/enumvalues_11.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['yellow_2177',['YELLOW',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4a8a568e5f41b7e4da88fe5c4a00aad34e',1,'CppCommon']]]
+  ['yellow_2179',['YELLOW',['../namespace_cpp_common.html#ac1ca6affe95577c60f18950cd8682db4a8a568e5f41b7e4da88fe5c4a00aad34e',1,'CppCommon']]]
 ];
