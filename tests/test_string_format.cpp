abe68e6699c291e6a2ebd3e4ad1296988c10c507
Ivan Shynkarenka <Ivan_Shynkarenka@epam.com>
2022-05-13 00:41:17 +0400

    Fix Visual Studio update

--- tests/test_string_format.cpp
+++ tests/test_string_format.cpp
@@ -28,12 +28,12 @@ private:
 template <>
 struct fmt::formatter<Date>
 {
-     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) { return ctx.begin(); }
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) { return ctx.begin(); }
 
     template <typename FormatContext>
     auto format(const Date& date, FormatContext& ctx) -> decltype(ctx.out())
     {
-        return format_to(ctx.out(), "{}-{}-{}", date.year(), date.month(), date.day());
+        return fmt::format_to(ctx.out(), "{}-{}-{}", date.year(), date.month(), date.day());
     }
 };
 
