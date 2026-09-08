c2816be4d608f0584f5af0f989a94d15d0779182
chronoxor <chronoxor@gmail.com>
2026-09-09 01:15:34 +0200

    Fixing compilation issue under Cygwin

--- include/system/dll.h
+++ include/system/dll.h
@@ -143,7 +143,7 @@ private:
 
     static const size_t StorageSize = 48;
     static const size_t StorageAlign = 8;
-    alignas(StorageAlign) std::array<std::byte, StorageSize> _storage;
+    alignas(StorageAlign) std::array<std::byte, StorageSize> _storage{};
 
     //! Resolve dynamic link library symbol by the given name
     /*!
